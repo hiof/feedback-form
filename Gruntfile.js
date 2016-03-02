2b7845a30b8767e3750e30a66f5481597479494d
Kenneth Dahlstrøm <kenneth@keda.no>
2016-03-02 14:42:06 +0100

    Initial version
    Signed-off-by: Kenneth Dahlstrøm <kenneth@keda.no>

---
+++ Gruntfile.js
@@ -0,0 +1,280 @@
+module.exports = function(grunt) {
+  // Loads each task referenced in the packages.json file
+  require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+  require('time-grunt')(grunt);
+
+  // Initiate grunt tasks
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    moment: require('moment'),
+    // Tasks
+    sass: {
+      options: {
+
+      },
+      dist: {
+        files: {
+          'build/feedback-form.css': 'app/assets/sass/feedback-form.scss'
+        }
+      }
+    },
+
+    autoprefixer: {
+      options: {
+        browsers: ['last 2 versions', 'ie 8', 'ie 9']
+        //diff: 'build/config/*.diff'
+      },
+      prefix: {
+        expand: true,
+        //flatten: true,
+        src: 'build/*.css'
+        //dest: 'tmp/css/prefixed/'
+      }
+    },
+    cssmin: {
+      main: {
+        options: {
+          banner: '/*! <%= pkg.name %> v<%= pkg.version %> by <%= pkg.author %>, released: <%= moment().format("HH:mm DD-MM-YYYY") %> */'
+        },
+        expand: true,
+        cwd: 'build',
+        src: ['*.css', '!*.min.css'],
+        dest: 'build/',
+        ext: '.v<%= pkg.version %>.min.css'
+      }
+    },
+    copy: {
+      dist: {
+        expand: true,
+        cwd: 'build/',
+        src: '**',
+        dest: 'dist',
+        filter: 'isFile'
+      }
+    },
+    clean: {
+      dist: ['dist/**/*'],
+      deploy: ['deploy/**/*'],
+      build: ['build/**/*']
+    },
+
+    eslint: {
+      options: {
+        //format: require('babel-eslint'),
+        quiet: true
+        //rulePath: ['node_modules/eslint-rules-es2015/lib/index.js']
+      },
+      target: ['app/assets/js/**/*.js']
+    },
+
+    handlebars: {
+      options: {
+        namespace: 'Hiof.Templates',
+        processName: function(filePath) {
+          if (filePath.substring(0, 4) === 'vend') {
+            return filePath.replace(/^vendor\/frontend\/app\/templates\//, '').replace(/\.hbs$/, '');
+          } else {
+            return filePath.replace(/^app\/templates\//, '').replace(/\.hbs$/, '');
+          }
+        }
+      },
+      all: {
+        files: {
+          "build/templates.js": ["app/templates/**/*.hbs"]
+        }
+      }
+    },
+    babel: {
+      options: {
+        sourceMap: true,
+        presets: ['es2015']
+      },
+      dist: {
+        files: {
+          'build/_<%= pkg.name %>.js': 'app/assets/js/_component_feedbackform.js'
+        }
+      }
+    },
+    concat: {
+      scripts: {
+        src: [
+          'vendor/jQuery-ajaxTransport-XDomainRequest/jquery.xdomainrequest.min.js',
+          'build/templates.js',
+          'build/_<%= pkg.name %>.js'
+        ],
+        dest: 'build/<%= pkg.name %>.v<%= pkg.version %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        mangle: false,
+        //compress: true,
+        preserveComments: false,
+        banner: '/*! <%= pkg.name %> v<%= pkg.version %> by <%= pkg.author %>, released: <%= moment().format("HH:mm DD-MM-YYYY") %>, license: <%= pkg.license %> */'
+      },
+      main: {
+        files: {
+          'build/<%= pkg.name %>.v<%= pkg.version %>.min.js': ['build/<%= pkg.name %>.v<%= pkg.version %>.js']
+        }
+      }
+    },
+    versioning: {
+      options: {
+        cwd: 'build/',
+        outputConfigDir: 'build/',
+        namespace: 'hiof'
+      },
+      build: {
+        files: [{
+          assets: [{
+            src: ['build/<%= pkg.name %>.v<%= pkg.version %>.min.js'],
+            dest: 'build/<%= pkg.name %>.v<%= pkg.version %>.min.js'
+          }],
+          key: 'assets',
+          dest: '',
+          type: 'js',
+          ext: '.min.js'
+        }, {
+          assets: [{
+            src: 'build/<%= pkg.name %>.v<%= pkg.version %>.min.css',
+            dest: 'build/<%= pkg.name %>.v<%= pkg.version %>.min.css'
+          }],
+          key: 'assets',
+          dest: '',
+          type: 'css',
+          ext: '.min.css'
+        }]
+      },
+      deploy: {
+        options: {
+          output: 'php',
+          outputConfigDir: 'build/',
+        },
+        files: [{
+          assets: [{
+            src: ['build/<%= pkg.name %>.v<%= pkg.version %>.min.js'],
+            dest: 'build/<%= pkg.name %>.v<%= pkg.version %>.min.js'
+          }],
+          key: 'assets',
+          dest: '',
+          type: 'js',
+          ext: '.min.js'
+        },
+
+        {
+          assets: [{
+            src: 'build/<%= pkg.name %>.v<%= pkg.version %>.min.css',
+            dest: 'build/<%= pkg.name %>.v<%= pkg.version %>.min.css'
+          }],
+          key: 'assets',
+          dest: '',
+          type: 'css',
+          ext: '.min.css'
+        }
+      ]
+    }
+  },
+  secret: grunt.file.readJSON('secret.json'),
+  sftp: {
+    stage: {
+      files: {
+        "./": "dist/**"
+      },
+      options: {
+        path: '<%= secret.stage.path %>',
+        srcBasePath: "dist/",
+        host: '<%= secret.stage.host %>',
+        username: '<%= secret.stage.username %>',
+        password: '<%= secret.stage.password %>',
+        //privateKey: grunt.file.read('id_rsa'),
+        //passphrase: '<%= secret.passphrase %>',
+        showProgress: true,
+        createDirectories: true,
+        directoryPermissions: parseInt(755, 8)
+      }
+    },
+    prod: {
+      files: {
+        "./": "dist/**"
+      },
+      options: {
+        path: '<%= secret.prod.path %>',
+        srcBasePath: "dist/",
+        host: '<%= secret.prod.host %>',
+        username: '<%= secret.prod.username %>',
+        password: '<%= secret.prod.password %>',
+        //privateKey: grunt.file.read('id_rsa'),
+        //passphrase: '<%= secret.passphrase %>',
+        showProgress: true,
+        createDirectories: true,
+        directoryPermissions: parseInt(755, 8)
+      }
+    },
+    stage2: {
+      files: {
+        "./": "dist/**"
+      },
+      options: {
+        path: '<%= secret.stage2.path %>',
+        srcBasePath: "dist/",
+        host: '<%= secret.stage2.host %>',
+        username: '<%= secret.stage2.username %>',
+        password: '<%= secret.stage2.password %>',
+        //privateKey: grunt.file.read('id_rsa'),
+        //passphrase: '<%= secret.passphrase %>',
+        showProgress: true,
+        createDirectories: true,
+        directoryPermissions: parseInt(755, 8)
+      }
+    },
+    prod2: {
+      files: {
+        "./": "dist/**"
+      },
+      options: {
+        path: '<%= secret.prod2.path %>',
+        srcBasePath: "dist/",
+        host: '<%= secret.prod2.host %>',
+        username: '<%= secret.prod2.username %>',
+        password: '<%= secret.prod2.password %>',
+        //privateKey: grunt.file.read('id_rsa'),
+        //passphrase: '<%= secret.passphrase %>',
+        showProgress: true,
+        createDirectories: true,
+        directoryPermissions: parseInt(755, 8)
+      }
+    }
+  }
+  //watch: {
+  //  hbs: {
+  //    files: ['app/templates/**/*.hbs'],
+  //    tasks: ['handlebars', 'copy:jstemplates'],
+  //    options: {
+  //      livereload: true,
+  //    },
+  //  },
+  //  js: {
+  //    files: ['app/assets/js/**/*.js', 'app/assets/js/**/*.json'],
+  //    tasks: ['jshint', 'concat:scripts', 'versioning:build'],
+  //    options: {
+  //      livereload: true,
+  //    },
+  //  },
+  //}
+
+});
+
+grunt.registerTask('subtaskJs', ['eslint', 'handlebars', 'babel', 'concat:scripts', 'uglify']);
+grunt.registerTask('subtaskCss', ['sass', 'autoprefixer', 'cssmin']);
+
+grunt.registerTask('build', ['clean:build', 'clean:dist', 'subtaskJs', 'subtaskCss', 'versioning:build']);
+grunt.registerTask('deploy', ['clean:build', 'clean:dist', 'subtaskJs', 'subtaskCss', 'versioning:deploy', 'copy:dist']);
+
+
+
+grunt.registerTask('deploy-staging', ['deploy', 'sftp:stage']);
+grunt.registerTask('deploy-prod', ['deploy', 'sftp:prod']);
+grunt.registerTask('deploy-staging2', ['deploy', 'sftp:stage2']);
+grunt.registerTask('deploy-prod2', ['deploy', 'sftp:prod2']);
+
+};
---
+++ app/assets/js/_component_feedbackform.js
@@ -0,0 +1,80 @@
+(function(Hiof, undefined) {
+
+    Hiof.setupFeedbackForm = function(ln) {
+
+        $.getJSON("/assets/js/data/i18n.json", function(data) {
+            var myData = data[ln]["feedback-form"];
+
+            var clientOptions = Hiof.options.client;
+
+            myData.clientLanguage = ln;
+            myData.clientUrl = clientOptions.url;
+            myData.clientBrowserName = clientOptions.browserName;
+            myData.clientBrowserVersion = clientOptions.browserVersion;
+            myData.clientOsName = clientOptions.osName;
+            myData.clientOsVersion = clientOptions.osVersion;
+            myData.clientViewportWidth = clientOptions.viewportWidth;
+            myData.clientViewportHeight = clientOptions.viewportHeight;
+
+
+            Hiof.appendFeedbackForm(myData);
+        });
+        //return feedbackForm;
+    };
+    Hiof.appendFeedbackForm = function(data) {
+        let templateSource = Hiof.Templates['forms/feedback'],
+            markup = templateSource(data);
+
+        $('#main').append(markup);
+        var formHeight = $('.feedback-form').height();
+        $('.feedback').css('height', formHeight + 'px');
+    };
+
+
+    $(function() {
+        var lang;
+        if ($('#main').attr('data-page-category') === "page") {
+
+            lang = $('html').attr('lang');
+
+            Hiof.setupFeedbackForm(lang);
+
+        } else if ($('#content').attr('data-page-category') === "page") {
+            lang = $('html').attr('lang');
+
+            Hiof.setupFeedbackForm(lang);
+        }
+
+
+        $('body').on('submit', '.feedback-form', function(e) {
+            e.preventDefault();
+
+            var formData = $('.feedback-form').serialize();
+            $.ajax({
+                url: "http://hiof.no/api/v1/feedback/",
+                type: "POST",
+                data: formData,
+                beforeSend: function() {
+                    $('.feedback-form fieldset').attr('disabled', 'disabled');
+                },
+                success: function(data, textStatus, jqXHR) {
+                    //data - response from server
+                    //debug('success:' + data);
+                    $('.feedback-form').slideToggle();
+                    $('.feedback-form-success').slideToggle();
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    $('.feedback-form fieldset').prop('disabled', false);
+                    //data - response from server
+                    //debug('error: ' + jqXHR + textStatus + errorThrown);
+                    //debug(textStatus);
+
+                }
+            });
+
+
+        });
+
+    });
+
+})(window.Hiof = window.Hiof || {});
